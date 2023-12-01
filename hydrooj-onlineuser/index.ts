4946a1adad6ed30e64ccd2768a209a0f19cb2a2d
LYkcul <126674875+LYkcul@users.noreply.github.com>
2023-12-02 01:07:08 +0800

    feat(hydrooj-onlineuser): init

---
+++ hydrooj-onlineuser/index.ts
@@ -0,0 +1,44 @@
+import {
+  Handler, Context, UserModel, db,
+} from 'hydrooj'
+
+const token = db.collection('token');
+
+class OnlineUserHandler extends Handler {
+  async get(domainId: string) {
+    console.log(domainId);
+    const res = await token.aggregate([
+      { $match: { updateAt: { $gte: new Date(Date.now() - 300 * 1000) }, uid: { $gt: 1 } } },
+      { $group: { _id: '$uid' } },
+      { $lookup: { from: 'user', localField: '_id', foreignField: '_id', as: 'user' } },
+      { $project: { _id: '$_id' } },
+      { $unwind: '$_id' },
+    ]).toArray();
+    const udict = [];
+    for (const doc of res) {
+      const udoc = await UserModel.getById('system', doc._id);
+      udict.push(udoc);
+    }
+    this.response.body = { udict };
+    this.response.template = 'onlineuser.html';
+  }
+}
+
+export async function apply(ctx: Context) {
+  ctx.Route('onlineuser', '/onlineuser', OnlineUserHandler);
+  ctx.i18n.load('zh', {
+    "No Online Users": '暂无在线用户',
+    "There are {0} users online.": '共有 {0} 位用户在线',
+    onlineuser: '在线用户',
+  });
+  ctx.i18n.load('zh_TW', {
+    "No Online Users": '暫無線上使用者',
+    "There are {0} users online.": '共有 {0} 位使用者線上',
+    onlineuser: '線上使用者',
+  });
+  ctx.i18n.load('kr', {
+    "No Online Users": '온라인 사용자 없어요',
+    "There are {0} users online.": '총 {0}명의 사용자가 온라인에 있어요',
+    onlineuser: '온라인 사용자',
+  });
+}
